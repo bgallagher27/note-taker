73c3dbccd545084f74f2c9ff29cd254527f0a846
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-14 16:23:42 -0400

    server variables

--- public/server.js
+++ public/server.js
@@ -1,13 +1,13 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 const dataBase = require("../db/db.json");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
