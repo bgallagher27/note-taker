374ae26f238230d24e532aca87eaffecbaab545a
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-14 16:22:55 -0400

    server route setup

--- public/server.js
+++ public/server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const dataBase = require("../db/db.json");
 
 // Sets up the Express App
 // =============================================================
@@ -14,14 +15,16 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+  res.sendFile(path.join(__dirname, "index.html"));
+});
   
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));;
-  });
+  res.sendFile(path.join(__dirname, "notes.html"));;
+});
   
-
+app.get("/api/notes", function(req, res) {
+  return res.json(dataBase);
+});
 
 // Starts the server to begin listening
 // =============================================================
